33e6b4c61824e3ec8dd12abd234dd79c2d44c49e
1161140118 <386506287@qq.com>
2018-10-19 09:05:07 +0800

    create project in eclipse

---
+++ src/HttpProxyServer.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.util.Scanner;
+
+
+/**
+ * @author chen
+ *
+ */
+public class HttpProxyServer {
+    public static Socket proxyServer;
+    public static SocketAddress proxyServerAddr;
+    public static int proxyPort = 10240;
+    public static 
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
